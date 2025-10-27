19be0f15b8e8f3253856bd9b69149a03524c6050
LeslyV <l3sly2022@gmail.com>
2025-10-26 21:04:46 -0700

    creating hw 3

---
+++ index.mjs
@@ -0,0 +1,35 @@
+import express from 'express';
+import fetch from 'node-fetch';
+
+import {createRequire} from 'module';
+const cjsImport = createRequire(import.meta.url);
+const billboard = cjsImport('billboard-top-100');
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//root route
+app.get('/', async(req, res) => {
+    res.render('home.ejs');
+}); // not sure what else to do here
+
+
+//music
+app.get('/music', (req, res) => {
+    //some stuff unsure yet
+});
+
+//movies
+app.get('/music', (req, res) => {
+    //some stuff unsure yet
+});
+
+//mashup
+app.get('/music', (req, res) => {
+    //some stuff unsure yet
+});
+
+app.listen(3000, () => {
+    console.log('server started');
+});
