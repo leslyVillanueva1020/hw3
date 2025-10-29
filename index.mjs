57c74926284c22467bd49c15a865631ee5d7f4d8
LeslyV <l3sly2022@gmail.com>
2025-10-28 21:53:48 -0700

    basic layout of nav, made all pages and have packages installed/api

--- index.mjs
+++ index.mjs
@@ -6,38 +6,56 @@ dotenv.config();
 console.log("TMDB key loaded:", process.env.TMDB_API_KEY ? 'yes' : 'no');
 const apiKey = process.env.TMDB_API_KEY;
 
-import {createRequire} from 'module';
-const cjsImport = createRequire(import.meta.url);
-const billboard = cjsImport('billboard-top-100');
+const GOSSIP_2000s =[
+    "Britney's 2007 comeback dominated headlines.",
+    "'TomKat' (Tom Cruise & Katie Holmes) was everywhere.",
+    "Lindsay Lohan's tabloid era defined mid-2000s gossip.",
+    "Bennifer 1.0 kept the paparazzi busy.",
+    "The Simple Life made celebutantes mainstream.",
+    "The 'Leave Britney Alone' moment went viral.",
+    "Paris Hilton's catchphrase 'That's hot' was ubiquitous.",
+    "Justin & Britney's denim outfits live rent-free online.",
+    "The 'Team Aniston vs. Team Jolie' era split magazines.",
+    "Madonna's 'Hung Up' era brought disco back."
+]
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-//root route
-app.get('/', async(req, res) => {
-    let year = 2005; //later will be randomized
-    let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_year=${year}&sort_by=popularity.desc`;
+// marks current route so nav can highlight the active heart
+app.use((req, res, next) => {
+  res.locals.currentPath = req.path; // '/', '/music', '/movies', '/mashup'
+  next();
+});
 
-    const reponse = await fetch(url);
-    const data = await reponse.json();
-    console.log(data.results[0]);
-    res.render('home.ejs');
+//root route
+app.get('/', async (req, res) => {
+    let randYear = Math.floor(Math.random() * 10) + 2000;
+    let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_year=${randYear}&sort_by=popularity.desc`;
+    let response = await fetch(url);
+    let data = await response.json(); //not doing anything yet with this data
+    // console.log(data);
+
+    let randgoss = Math.floor(Math.random() * 10);
+    let selectedGossip = GOSSIP_2000s[randgoss];
+
+    res.render('home.ejs', {title: "Home", gossip: selectedGossip});
 });
 
 //music
 app.get('/music', (req, res) => {
-    //some stuff unsure yet
+    res.render('music.ejs', {title: "Music"});
 });
 
 //movies
-app.get('/music', (req, res) => {
-    //some stuff unsure yet
+app.get('/movies', (req, res) => {
+    res.render('movies.ejs', {title:"Movies"});
 });
 
 //mashup
-app.get('/music', (req, res) => {
-    //some stuff unsure yet
+app.get('/mashup', (req, res) => {
+    res.render('mashup.ejs', {title:"Mashup Time"});
 });
 
 app.listen(3000, () => {
